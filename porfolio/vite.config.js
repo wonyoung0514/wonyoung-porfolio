35f82caa6ed81a40bb100b7e9be663fdff05b990
wonyoung <wonyoung@kaywon.ac.kr>
2025-12-05 21:14:15 +0900

    GitHub Pages base 설정 수정 및 재빌드

--- porfolio/vite.config.js
+++ porfolio/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./src", 
-  base: "./",    
+  root: "./src",
+  base: "/wonyoung-porfolio/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
     open: true,
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
   },
 });
